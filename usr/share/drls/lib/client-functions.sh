12c99216cab2fbd51b725b7e1a00e1a0c392baa5
krbu <ruben@brainupdaters.net>
2013-11-26 14:46:31 +0100

    Afegides les funcions a client-functions.sh

--- usr/share/drls/lib/client-functions.sh
+++ usr/share/drls/lib/client-functions.sh
@@ -1,67 +1,94 @@
 # file with default client functions to implant.
-# CLIENTDB="$VAR_DIR/client.data"
+ CLIENTDB="$VAR_DIR/client.data"
 
 function exist_client_id () {
   local CLI_ID=$1
-# Check if parameter $1 is ok and if exists client with this id in database. Return 0 for ok, return 1 not ok.     
+  grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $1}'|grep $CLI_ID &> /dev/null
+  if [ $? == 0 ];then return 0; else return 1; fi
+  
+# Check if parameter $1 is ok and if exists client with this id in database. Return 0 for ok, return 1 not ok.
 }
 
 function exist_client_name(){
   local CLI_NAME=$1
-# Check if parameter $1 is ok and if exists client with this name in database. Return 0 for ok, return 1 not ok.
+  grep -w $CLI_NAME $CLIENTDB|awk -F":" '{print $2}'|grep $CLI_NAME &> /dev/null
+  if [ $? == 0 ];then return 0; else return 1; fi
+
+# Check if parameter $1 is ok and if exists client with this name in database. Return 0 for ok
+, return 1 not ok.
 }
 
 function get_cient_id_by_name(){
   local CLI_NAME=$1
 # Check if parameter $1 is ok
-  if ( exist_client_name "$CLI_NAME" )
-  then
-# Get client id from database and return it
-  else
-# Error client not exist "exit X"?
+  grep -w $CLI_NAME $CLIENTDB |awk -F":" '{print $2}'|grep $CLI_NAME &> /dev/null 
+  if [ $? == 0 ]
+  then 
+	# Get client id from database and return it
+	CLI_ID=`grep -w $CLI_NAME $CLIENTDB|awk -F":" '{print $1}'`
+	echo $CLI_ID
+  else 
+	# Error client not exist "exit X"?
+	LogPrint "ERROR: Client not exist"
+	exit 1
   fi
 }
 
 function get_client_ip(){
   local CLI_ID=$1
 # Check if parameter $1 is ok
-  if ( exist_client_id "$CLI_ID" ) 
+  grep -w $CLI_ID $CLIENTDB |awk -F":" '{print $1}'|grep $CLI_ID &> /dev/null
+  if [ $? == 0 ]
   then
-# Get client ip from database and return it
+	# Get client ip from database and return it
+	CLI_IP=`grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $4}'`
+	echo $CLI_IP
   else
-# Error client not exist "exit X"?
+	# Error client not exist "exit X"?
+	LogPrint "ERROR: Client not exist"
+	exit 1
   fi
 }
 
 function get_client_name(){
-  local CLI_ID=$1
-# Check if parameter $1 is ok
-  if ( exist_client_id "$CLI_ID" ) 
-  then
-# Get client name from database and return it
-  else
-# Error client not exist "exit X"?
-  fi
+  local CLI_NAME=$1
+  #Check if parameter $1 is ok and if exists client with this name in database. Return 0 for ok
+  grep -w $CLI_NAME $CLIENTDB|awk -F":" '{print $2}'|grep $CLI_NAME &> /dev/null
+  if [ $? == 0 ];then return 0; else return 1; fi
+  # Check if parameter $1 is ok and if exists client with this name in database. Return 0 for ok
 }
 
 function get_client_mac(){
-  local CLI_ID=$1
-# Check if parameter $1 is ok
-  if ( exist_client_id "$CLI_ID" ) 
+ local CLI_ID=$1
+  # Check if parameter $1 is ok
+  grep -w $CLI_ID $CLIENTDB |awk -F":" '{print $1}'|grep $CLI_ID &> /dev/null
+  if [ $? == 0 ]
   then
-# Get client mac from database and return it
+	# Get client mac from database and return it
+	CLI_MAC=`grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $3}'`
+	echo $CLI_MAC	
   else
-# Error client not exist "exit X"?
+	# Error client not exist "exit X"?
+	LogPrint "ERROR: Client not exist"
+	exit 1
   fi
 }
 
-function check_client_connectivity(){
+function check_client_connectivity () {
   local CLI_ID=$1
 # Check if parameter $1 is ok
-  if ( exist_client_id "$CLI_ID" ) 
+  grep -w $CLI_ID $CLIENTDB |awk -F":" '{print $1}'|grep $CLI_ID &> /dev/null
+  if [ $? == 0 ]
   then
-# Chek if client is available. Return 0 for ok, return 1 not ok.
+	# Chek if client is available. Return 0 for ok, return 1 not ok.
+  	CLI_IP=`grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $4}'`
+	ping  -c 1 -t 2 $CLI_IP &>/dev/null
+	if [ $? -eq 0 ];then return 0; else return 1;fi
   else
-# Error client not exist "exit X"?
+	# Error client not exist "exit X"?
+	LogPrint "ERROR: Client not exist"
+	exit 1
+
   fi
 }
+
