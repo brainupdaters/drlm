0fb52d90c4140f22a43770798382b050f1ac2148
proura <pau@brainupdaters.net>
2013-11-29 19:06:44 +0100

    deleted some control steps

--- usr/share/drls/backup/run/default/019_check_client_backup_reqs.sh
+++ usr/share/drls/backup/run/default/019_check_client_backup_reqs.sh
@@ -1,27 +1,19 @@
-
 # Check if the target client for backup is in DRLS client database
 
 if test -n "$CLINAME"; then
-	echo "by cliname"
-	
 	if exist_client_name "$CLINAME" ;	
 	then
-		echo "h1"
 		IDCLIENT=$(get_cient_id_by_name $CLINAME)
 		CLIMACADDR=$(get_client_mac $IDCLIENT)
 	        CLIIPADDR=$(get_client_ip $IDCLIENT)
-		echo "h2"
 	else
 		StopIfError "$PROGRAM: Client named: $CLINAME not registered!"
 	fi
 else
-	echo "by clientid"
         if exist_client_id "$IDCLIENT" ;
         then
-		echo "h3"
         	CLIMACADDR=$(get_client_mac $IDCLIENT)
 	        CLIIPADDR=$(get_client_ip $IDCLIENT)
-		echo "h4"
         else
         	StopIfError "$PROGRAM: Client with ID: $IDCLIENT not registered!"
         fi
