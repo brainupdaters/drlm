ee2c6e1a6259952682689e18dca97c9ce169436f
proura <pau@brainupdaters.net>
2013-11-29 23:01:06 +0100

    Client data file variable updated to default.conf variable

--- usr/share/drls/lib/client-functions.sh
+++ usr/share/drls/lib/client-functions.sh
@@ -1,9 +1,9 @@
 # file with default client functions to implant.
- CLIENTDB="$VAR_DIR/client.data"
+# $CLIDB is the defaul.conf variable of Client file
 
 function exist_client_id () {
   local CLI_ID=$1
-  grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $1}'|grep $CLI_ID &> /dev/null
+  grep -w $CLI_ID $CLIDB|awk -F":" '{print $1}'|grep $CLI_ID &> /dev/null
   if [ $? == 0 ];then return 0; else return 1; fi
   
 # Check if parameter $1 is ok and if exists client with this id in database. Return 0 for ok, return 1 not ok.
@@ -11,7 +11,7 @@ function exist_client_id () {
 
 function exist_client_name(){
   local CLI_NAME=$1
-  grep -w $CLI_NAME $CLIENTDB|awk -F":" '{print $2}'|grep $CLI_NAME &> /dev/null
+  grep -w $CLI_NAME $CLIDB|awk -F":" '{print $2}'|grep $CLI_NAME &> /dev/null
   if [ $? == 0 ];then return 0; else return 1; fi
 
 # Check if parameter $1 is ok and if exists client with this name in database. Return 0 for ok
@@ -21,11 +21,11 @@ function exist_client_name(){
 function get_cient_id_by_name(){
   local CLI_NAME=$1
 # Check if parameter $1 is ok
-  grep -w $CLI_NAME $CLIENTDB |awk -F":" '{print $2}'|grep $CLI_NAME &> /dev/null 
+  grep -w $CLI_NAME $CLIDB|awk -F":" '{print $2}'|grep $CLI_NAME &> /dev/null 
   if [ $? == 0 ]
   then 
 	# Get client id from database and return it
-	CLI_ID=`grep -w $CLI_NAME $CLIENTDB|awk -F":" '{print $1}'`
+	CLI_ID=`grep -w $CLI_NAME $CLIDB|awk -F":" '{print $1}'`
 	echo $CLI_ID
   else 
 	# Error client not exist "exit X"?
@@ -40,7 +40,7 @@ function get_client_ip(){
   if exist_client_id "$CLI_ID" ;
   then
 	# Get client ip from database and return it
-	CLI_IP=`grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $4}'`
+	CLI_IP=`grep -w $CLI_ID $CLIDB|awk -F":" '{print $4}'`
 	echo $CLI_IP
   else
 	# Error client not exist "exit X"?
@@ -55,7 +55,7 @@ function get_client_name(){
   if exist_client_id "$CLI_ID" ;
   then
 	# Get client name from database and return it
-	CLI_NAME=`grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $2}'`
+	CLI_NAME=`grep -w $CLI_ID $CLIDB|awk -F":" '{print $2}'`
 	echo $CLI_NAME
   else
 	# Error client not exist "exit X"?
@@ -70,7 +70,7 @@ function get_client_mac(){
   if exist_client_id "$CLI_ID" ;
   then
 	# Get client mac from database and return it
-	CLI_MAC=`grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $3}'`
+	CLI_MAC=`grep -w $CLI_ID $CLIDB|awk -F":" '{print $3}'`
 	echo $CLI_MAC	
   else
 	# Error client not exist "exit X"?
@@ -85,7 +85,7 @@ function check_client_connectivity () {
   if exist_client_id "$CLI_ID" ;
   then
 	# Chek if client is available. Return 0 for ok, return 1 not ok.
-  	CLI_IP=`grep -w $CLI_ID $CLIENTDB|awk -F":" '{print $4}'`
+  	CLI_IP=`grep -w $CLI_ID $CLIDB|awk -F":" '{print $4}'`
 	ping  -c 1 -t 2 $CLI_IP &>/dev/null
 	if [ $? -eq 0 ];then return 0; else return 1;fi
   else
