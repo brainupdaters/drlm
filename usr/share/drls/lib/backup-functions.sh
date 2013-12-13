3ad306da5c40fcd3ad9d63c8d298af46f60c3de9
proura <pau@brainupdaters.net>
2013-12-13 18:46:22 +0100

    Create backup-functions.sh

---
+++ usr/share/drls/lib/backup-functions.sh
@@ -0,0 +1,11 @@
+# file with default backup functions to implement.
+
+function run_mkbackup_ssh_remote () {
+  local CLI_ID=$1
+#returns stdo of ssh  
+}
+
+function run_mkrescue_ssh_remote () {
+  local CLI_ID=$1
+#returns stdo of ssh
+}
