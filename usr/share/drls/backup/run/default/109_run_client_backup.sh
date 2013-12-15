5f34e1c41f7b0feea948131b1423e1dd564a88b8
proura <pau@brainupdaters.net>
2013-12-16 00:39:11 +0100

    new 109_run_client_backup.sh with functions

---
+++ usr/share/drls/backup/run/default/109_run_client_backup.sh
@@ -0,0 +1,11 @@
+if run_mkbackup_ssh_remote $IDCLIENT ;
+then
+   LogPrint "${CLIENT}: Backup Succesful!"
+else
+   report_error $?
+   rm -vf ${PXEDIR}/${CLINAME}/.lockfile ${PXEDIR}/${CLINAME}/*
+   rm -vf ${BKPDIR}/${CLINAME}/*     
+   tar -C ${PXEDIR}/${CLINAME} -xf ${PXEDIR}/${CLINAME}/.archive/${CLINAME}.${PXEDATE}.pxe.arch
+   tar -C ${BKPDIR}/${CLINAME} -xf ${BKPDIR}/${CLINAME}/.archive/${CLINAME}.${BKPDATE}.bkp.arch
+   StopIfError "Backup Failed! See log ${LOGFILE} for details"
+fi
