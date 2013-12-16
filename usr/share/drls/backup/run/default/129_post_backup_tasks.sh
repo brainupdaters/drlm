d18eaf119492fdf6f5035dffb4b7a336769353b3
proura <pau@brainupdaters.net>
2013-12-16 02:12:38 +0100

    Update 129_post_backup_tasks.sh

--- usr/share/drls/backup/run/default/129_post_backup_tasks.sh
+++ usr/share/drls/backup/run/default/129_post_backup_tasks.sh
@@ -5,7 +5,7 @@
                 chmod 755 ${PXEDIR}/${CLINAME}
         fi
 
-        if [ $(stat -c %a ${PXEDIR}/${CLINAME}/${CLINAME}.kernel) != "755") ]
+        if [ $(stat -c %a ${PXEDIR}/${CLINAME}/${CLINAME}.kernel) != "755" ]
         then
                 chmod 755 ${PXEDIR}/${CLINAME}/${CLINAME}.kernel
         fi
