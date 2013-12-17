119a91beeb4c1cad87167224b4ef3532761539eb
proura <pau@brainupdaters.net>
2013-12-17 15:59:47 +0100

    Simplified Code

--- usr/share/drls/backup/run/default/059_prepare_backup_preservation.sh
+++ usr/share/drls/backup/run/default/059_prepare_backup_preservation.sh
@@ -1,77 +1,46 @@
-
 #PRE RUN BACKUP
 
 if [ -d ${PXEDIR}/${CLINAME} ]
 then
-        if [ -d ${PXEDIR}/${CLINAME}/.archive ]
+        if [ ! -d ${PXEDIR}/${CLINAME}/.archive ]
         then
-		if [ -f ${PXEDIR}/${CLINAME}/${CLINAME}.kernel ]
-		then
-                 	PXETIME=$(stat -c %y ${PXEDIR}/${CLINAME}/${CLINAME}.kernel | awk '{print $2}' | awk -F"." '{print $1}' | tr -d ":")
-                	PXEDATE=$(stat -c %y ${PXEDIR}/${CLINAME}/${CLINAME}.kernel | awk '{print $1}' | tr -d "-")
-                	tar --exclude='.archive' -C ${PXEDIR}/${CLINAME} . -cf ${PXEDIR}/${CLINAME}/.archive/${CLINAME}.${PXEDATE}${PXETIME}.pxe.arch
-			if [ $? -eq 0 ]
-			then
-                		rm -vf ${PXEDIR}/${CLINAME}/.lockfile ${PXEDIR}/${CLINAME}/*
-			else
-				rm -vf ${PXEDIR}/${CLINAME}/.archive/${CLINAME}.$PXEDATE.pxe.arch
-				StopIfError "Problem archiving previous DR imagebackup. See log ${LOGFILE} for details"
-			fi
-		fi
-        else
-		if [ -f ${PXEDIR}/${CLINAME}/${CLINAME}.kernel ]
-		then
-                	mkdir -v ${PXEDIR}/${CLINAME}/.archive
-                 	PXETIME=$(stat -c %y ${PXEDIR}/${CLINAME}/${CLINAME}.kernel | awk '{print $2}' | awk -F"." '{print $1}' | tr -d ":")
-                	PXEDATE=$(stat -c %y ${PXEDIR}/${CLINAME}/${CLINAME}.kernel | awk '{print $1}' | tr -d "-")
-                	tar --exclude='.archive' -C ${PXEDIR}/${CLINAME} . -cf ${PXEDIR}/${CLINAME}/.archive/${CLINAME}.${PXEDATE}${PXETIME}.pxe.arch
-			if [ $? -eq 0 ]
-			then
-                		rm -vf ${PXEDIR}/${CLINAME}/.lockfile ${PXEDIR}/${CLINAME}/*
-			else
-				rm -vf ${PXEDIR}/${CLINAME}/.archive/${CLINAME}.${PXEDATE}${PXETIME}.pxe.arch
-				StopIfError "Problem archiving previous DR imagebackup. See log ${LOGFILE} for details"
-			fi
-		fi
+        	mkdir -v ${PXEDIR}/${CLINAME}/.archive
         fi
-#else
-        #do nothing...
+		
+	if [ -f ${PXEDIR}/${CLINAME}/${CLINAME}.kernel ]
+	then
+                PXETIME=$(stat -c %y ${PXEDIR}/${CLINAME}/${CLINAME}.kernel | awk '{print $2}' | awk -F"." '{print $1}' | tr -d ":")
+                PXEDATE=$(stat -c %y ${PXEDIR}/${CLINAME}/${CLINAME}.kernel | awk '{print $1}' | tr -d "-")
+                tar --exclude='.archive' -C ${PXEDIR}/${CLINAME} . -cf ${PXEDIR}/${CLINAME}/.archive/${CLINAME}.${PXEDATE}${PXETIME}.pxe.arch
+		if [ $? -eq 0 ]
+		then
+                	rm -vf ${PXEDIR}/${CLINAME}/.lockfile ${PXEDIR}/${CLINAME}/*
+		else
+			rm -vf ${PXEDIR}/${CLINAME}/.archive/${CLINAME}.$PXEDATE.pxe.arch
+			StopIfError "Problem archiving previous DR imagebackup. See log ${LOGFILE} for details"
+		fi
+	fi
 fi
 
 if [ -d ${BKPDIR}/${CLINAME} ]
 then
-        if [ -d ${BKPDIR}/${CLINAME}/.archive ]
+        if [ ! -d ${BKPDIR}/${CLINAME}/.archive ]
         then
-		if [ -f ${BKPDIR}/${CLINAME}/backup.tar.gz ]
+        	mkdir -v ${BKPDIR}/${CLINAME}/.archive
+        fi
+        
+	if [ -f ${BKPDIR}/${CLINAME}/backup.tar.gz ]
+	then
+               	BKPTIME=$(stat -c %y ${BKPDIR}/${CLINAME}/backup.tar.gz | awk '{print $2}' | awk -F"." '{print $1}' | tr -d ":")
+               	BKPDATE=$(stat -c %y ${BKPDIR}/${CLINAME}/backup.tar.gz | awk '{print $1}' | tr -d "-")
+               	tar --exclude='.archive' -C ${BKPDIR}/${CLINAME} . -cf ${BKPDIR}/${CLINAME}/.archive/${CLINAME}.${BKPDATE}${BKPTIME}.bkp.arch
+		if [ $? -eq 0 ]
 		then
-                	BKPTIME=$(stat -c %y ${BKPDIR}/${CLINAME}/backup.tar.gz | awk '{print $2}' | awk -F"." '{print $1}' | tr -d ":")
-                	BKPDATE=$(stat -c %y ${BKPDIR}/${CLINAME}/backup.tar.gz | awk '{print $1}' | tr -d "-")
-                	tar --exclude='.archive' -C ${BKPDIR}/${CLINAME} . -cf ${BKPDIR}/${CLINAME}/.archive/${CLINAME}.${BKPDATE}${BKPTIME}.bkp.arch
-			if [ $? -eq 0 ]
-			then
-                		rm -vf ${BKPDIR}/${CLINAME}/*
-			else
-				rm -vf ${BKPDIR}/${CLINAME}/.archive/${CLINAME}.${BKPDATE}${BKPTIME}.bkp.arch
-				StopIfError "Problem archiving previous DR backup. See log ${LOGFILE} for details"
-			fi
+               		rm -vf ${BKPDIR}/${CLINAME}/*
+		else
+			rm -vf ${BKPDIR}/${CLINAME}/.archive/${CLINAME}.${BKPDATE}${BKPTIME}.bkp.arch
+			StopIfError "Problem archiving previous DR backup. See log ${LOGFILE} for details"
 		fi
-        else
-		if [ -f ${BKPDIR}/${CLINAME}/backup.tar.gz ]
-		then
-                	mkdir -v ${BKPDIR}/${CLINAME}/.archive
-                	BKPTIME=$(stat -c %y ${BKPDIR}/${CLINAME}/backup.tar.gz | awk '{print $2}' | awk -F"." '{print $1}' | tr -d ":")
-                	BKPDATE=$(stat -c %y ${BKPDIR}/${CLINAME}/backup.tar.gz | awk '{print $1}' | tr -d "-")
-                	tar --exclude='.archive' -C ${BKPDIR}/${CLINAME} . -cf ${BKPDIR}/${CLINAME}/.archive/${CLINAME}.${BKPDATE}${BKPTIME}.bkp.arch
-			if [ $? -eq 0 ]
-			then
-                		rm -vf ${BKPDIR}/${CLINAME}/*
-			else
-				rm -vf ${BKPDIR}/${CLINAME}/.archive/${CLINAME}.${BKPDATE}${BKPTIME}.bkp.arch
-				StopIfError "Problem archiving previous DR backup. See log ${LOGFILE} for details"
-			fi
-        	fi
 	fi
-#else
-        #do nothing...
 fi
 
