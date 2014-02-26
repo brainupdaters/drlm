5fcbf6a0c58c3f51c8450b93298f54b3ad0abe80
Didac Oliveira <didac@brainupdaters.net>
2014-02-26 12:30:40 +0100

    NFS exports file generation

---
+++ usr/share/drls/lib/nfs-functions.sh
@@ -0,0 +1,52 @@
+# file with default nfs functions to implement.
+# $NETDB is the default.conf variable of Network file
+# $CLIDB is the default.conf variable of Client file
+# $NFS_DIR is the default.conf variable of dhcp dir file
+# $NFS_FILE is the default.conf variable of dhcp configuration file
+
+
+function generate_nfs_exports() {
+
+cp $NFS_FILE $NFS_DIR/exports.bkp
+cat /dev/null > $NFS_FILE
+
+for NFS_STORE in $PXEDIR $BKPDIR ; do
+
+	local SCOUNT=1
+	local CCOUNT=1
+	local NCLI=$(cat $CLIDB | grep -v "^#" | wc -l)
+
+	for CLIENT in $(cat $CLIDB | grep -v "^#") ; do
+	   local CLI_ID=$(echo $CLIENT | awk -F":" '{print $1}')
+	   local CLI_NAME=$(echo $CLIENT | awk -F":" '{print $2}')
+	   local CLI_MAC=$(echo $CLIENT | awk -F":" '{print $3}')
+	   local CLI_IP=$(echo $CLIENT | awk -F":" '{print $4}')
+	   local CLI_OS=$(echo $CLIENT | awk -F":" '{print $5}')
+	   local CLI_NET=$(echo $CLIENT | awk -F":" '{print $6}')
+
+	if [ $SCOUNT -eq 1 ]; then
+		echo "$NFS_STORE $CLI_NAME(rw,sync,no_root_squash) \\" | tee -a $NFS_FILE
+		let SCOUNT=SCOUNT+1
+		let CCOUNT=CCOUNT+1
+	else
+		if [ $CCOUNT -lt $NCLI ]; then
+			echo "	$CLI_NAME(rw,sync,no_root_squash) \\" | tee -a $NFS_FILE
+			let CCOUNT=CCOUNT+1
+		else
+			echo "	$CLI_NAME(rw,sync,no_root_squash)" | tee -a $NFS_FILE
+		fi
+	fi
+  
+	done
+done
+#Generates the nfs configuration file from CLIDB
+}
+
+function reload_nfs() {
+	exportfs -va
+	if [ $? -ne 0 ]; then
+		mv $NFS_DIR/exports.bkp $NFS_FILE
+		exportfs -va
+	fi
+}
+
