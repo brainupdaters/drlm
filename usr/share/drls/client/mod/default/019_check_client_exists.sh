47f813cb4d174ebcb643125c8eba7f0ef5a03137
proura <pau@brainupdaters.net>
2014-03-02 14:39:26 +0100

    Update 019_check_client_exists.sh

--- usr/share/drls/client/mod/default/019_check_client_exists.sh
+++ usr/share/drls/client/mod/default/019_check_client_exists.sh
@@ -4,26 +4,24 @@ Log "####################################################"
 
 
 # Check if the client is in DRLS client database
-if test -n "$CLINAME"; then
-        Log "Checking if client name: ${CLINAME} is registered in DRLS database ..."
-        if exist_client_name "$CLINAME" ;
+if test -n "$CLI_NAME"; then
+        Log "Checking if client name: ${CLI_NAME} is registered in DRLS database ..."
+        if exist_client_name "$CLI_NAME" ;
         then
-                CLI_ID=$(get_client_id_by_name $CLINAME)
-                CLI_IP=$(get_client_ip $CLI_ID)
-                Log "${CLINAME} found in DRLS database!"
+                CLI_ID=$(get_client_id_by_name $CLI_NAME)
+                Log "${CLI_NAME} found in DRLS database!"
         else
-                report_error "$PROGRAM: Client named: $CLINAME not registered!"
-                Error "$PROGRAM: Client named: $CLINAME not registered!"
+                report_error "$PROGRAM: Client named: $CLI_NAME not registered!"
+                Error "$PROGRAM: Client named: $CLI_NAME not registered!"
         fi
 else
-        Log "Checking if client ID: ${IDCLIENT} is registered in DRLS database ..."
-        if exist_client_id "$IDCLIENT" ;
+        Log "Checking if client ID: ${CLI_ID} is registered in DRLS database ..."
+        if exist_client_id "$CLI_ID" ;
         then
-                CLI_NAME=$(get_client_name $IDCLIENT)
-                CLI_IP=$(get_client_ip $IDCLIENT)
-                Log "${IDCLIENT} found in DRLS database!"
+                CLI_NAME=$(get_client_name $CLI_ID)
+                Log "${CLI_ID} found in DRLS database!"
         else
-                report_error "$PROGRAM: Client with ID: $IDCLIENT not registered!"
-                Error "$PROGRAM: Client with ID: $IDCLIENT not registered!"
+                report_error "$PROGRAM: Client with ID: $CLI_ID not registered!"
+                Error "$PROGRAM: Client with ID: $CLI_ID not registered!"
         fi
 fi
