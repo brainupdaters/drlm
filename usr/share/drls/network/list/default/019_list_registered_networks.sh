a8a5a706effd9f63cf1792fc99c17444f64185fe
krbu <ruben@brainupdaters.net>
2014-03-04 11:46:49 +0100

    019_list_registered_networks.sh

--- usr/share/drls/network/list/default/019_list_registered_networks.sh
+++ usr/share/drls/network/list/default/019_list_registered_networks.sh
@@ -1,8 +1,15 @@
 Log "####################################################"
 Log "# List of networks : 	                         "
 Log "####################################################"
-if exist_network_name "$NET_NAME" 
+if ! exist_network_name "$NET_NAME" 
 then
+	if [ "$NET_NAME" == "all" ]
+	then
+        	list_clients NET
+	else
+		printf '%25s\n' "$(tput bold)$CLI_NAME$(tput sgr0) not found in database!!"	
+	fi
+else
 	NET_ID=$(get_network_id_by_name $NET_NAME)
 	NET_IP=$(get_network_ip $NET_ID)
 	NET_MASK=$(get get_network_mask $NET_ID)
@@ -11,15 +18,6 @@ then
 	NET_DNS=$(get_network_dns $NET_ID)
 	NET_BRO=$(get_network_bcast $NET_ID)
 	NET_SRV=$(get_network_srv $NET_ID)
-	clear
 	client_list_tittle NET
 	printf '%15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s\n' "" "$NET_ID" "$NET_IP" "NET_MASK" "$NET_GW" "$NET_DO" "$NET_DNS" "$NET_BRO" "$NET_SRV" "$NET_NAME"
-else
-	clear
-	printf '%25s\n' "$(tput bold)$CLI_NAME$(tput sgr0) not found in database!!"
 fi
-if [ "$NET_NAME" == "all" ]
-then
-	list_clients NET
-fi
-
