e71426c4adfdac9f568d3dd1610f0f053c5220ad
Didac Oliveira <didac@brainupdaters.net>
2014-03-18 16:52:53 +0100

    Added new functons to backup-functions

--- usr/share/drls/lib/backup-functions.sh
+++ usr/share/drls/lib/backup-functions.sh
@@ -83,3 +83,96 @@ function list_backup () {
         if [ $CLI_ID -eq $CLI_BAC_ID ]; then printf '%-15s %-15s %-20s %-15s\n' "$BAC_ID" "$CLI_NAME" "$BAC_DATE" "$BAC_STAT"; fi
   done
 }
+
+function enable_loop_ro() {
+	local LO_DEV="/dev/loop${1}"
+	local DR_FILE=$2
+	
+	/sbin/losetup -r ${LO_DEV} ${ARCHDIR}/${DR_FILE} >> /dev/null &2>1  
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function enable_loop_rw() {
+	local LO_DEV="/dev/loop${1}"
+	local DR_FILE=$2
+	
+	/sbin/losetup ${LO_DEV} ${ARCHDIR}/${DR_FILE} >> /dev/null &2>1  
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function disable_loop() {
+	local LO_DEV=$1
+	
+	/sbin/losetup -d ${LO_DEV} >> /dev/null &2>1
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function do_mount_ro() {
+	local LO_DEV="/dev/loop${1}"
+	local CLI_NAME=$2
+	
+	/bin/mount -t ext2 -o ro ${LO_DEV} ${STORDIR}/${CLI_NAME} >> /dev/null &2>1
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function do_mount_rw() {
+	local LO_DEV="/dev/loop${1}"
+	local CLI_NAME=$2
+	
+	/bin/mount -t ext2 -o rw ${LO_DEV} ${STORDIR}/${CLI_NAME} >> /dev/null &2>1
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function do_umount() {
+	local LO_DEV=$1
+	
+	/bin/umount ${LO_DEV} >> /dev/null &2>1
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function do_umount_force() {
+	local LO_DEV=$1
+	
+	/bin/umount -f ${LO_DEV} >> /dev/null &2>1
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function enable_backup_db() {
+	local BKP_ID=$1
+	
+	ex -s -c ":/^${A_BKP_ID}/s/false/true/g" -c ":wq" ${BKPDB}
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function disable_backup_db() {
+	local BKP_ID=$1
+	
+	ex -s -c ":/^${BKP_ID}/s/true/false/g" -c ":wq" ${BKPDB}
+	if [ $? -eq 0 ]; then return 0; else return 1; fi
+
+# Return 0 if OK or 1 if NOK
+}
+
+function get_active_cli_bkp_from_db() {
+	local CLI_NAME=$1
+	local BKP_ID=$(grep -w ${CLI_NAME} ${BKPDB} | awk -F":" '{print $1,$5}'| grep -w "true" | awk '{print $1}')
+	echo $BKP_ID
+
+# Return Active Backup ID or Null string
+}
