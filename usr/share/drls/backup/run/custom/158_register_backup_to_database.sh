6410deb0e82f9cea5582dc177f9cba9f4dc16c73
Didac Oliveira <didac@brainupdaters.net>
2014-03-23 23:42:16 +0100

    Improved code and error handling in 158 208 runbackup scripts

--- usr/share/drls/backup/run/custom/158_register_backup_to_database.sh
+++ usr/share/drls/backup/run/custom/158_register_backup_to_database.sh
@@ -1,27 +1,11 @@
-Log "Registering DR backup to DRLS database ..."
+Log "$PROGRAM:$WORKFLOW:DB:Backup(${BKP_ID}):${CLI_NAME}: Registering DR backup to DRLS database .... " 
 
-# MARK LAST ACTIVE BACKUP AS INACTIVE
-
-A_BKP_ID=$(grep -w ${CLI_NAME} ${BKPDB} | awk -F":" '{print $1,$5}'| grep -w "true" | awk '{print $1}')
-if [ -n "$A_BKP_ID" ]; then
-	ex -s -c ":/^${A_BKP_ID}/s/true/false/g" -c ":wq" ${BKPDB}
-	if [ $? -eq 0 ]; then
-		Log "Previous ${CLI_NAME} DR backup (ID: ${A_BKP_ID}) tagged as inactive in database ..."
-	else
-		Error "Previous ${CLI_NAME} DR backup (ID: ${A_BKP_ID}) can not be tagged as inactive! Command: (ex -s -c ":/^${A_BKP_ID}/s/true/false/g" -c ":wq" ${BKPDB}) Failed!"
-	fi
+if register_backup ${BKP_ID} ${CLI_ID} ${CLI_NAME} ${DR_FILE} ${BKP_MODE} ;
+then
+	Log "$PROGRAM:$WORKFLOW:DB:insert:Backup(${BKP_ID}):${CLI_NAME}: .... Success!"
+else
+	report_error "ERROR:$PROGRAM:$WORKFLOW:DB:INSERT:Backup(${BKP_ID}):${CLI_NAME}: Problem registering backup on database! aborting ..."
+	Error "$PROGRAM:$WORKFLOW:DB:insert:Backup(${BKP_ID}):${CLI_NAME}: Problem registering backup on database! aborting ..."
 fi
 
-
-# REGISTER BACKUP TO DATABASE
-
-A_BKP=$(grep -w ${CLI_NAME} ${BKPDB} | grep -v "false" | wc -l)
-
-if [ $A_BKP -eq 0 ]; then
-	echo "${BKP_ID}:${CLI_ID}:${DR_NAME}::true:::" | tee -a ${BKPDB}
-	if [ $? -eq 0 ]; then
-		Log "DR backup for ${CLI_NAME} registered successfully to DRLS database!" 
-	else
-		report_error "Failed to register DR backup for client: ${CLI_NAME}! (${BKP_ID}:${CLI_ID}:${DR_NAME}::true:::) ... See log ${LOGFILE} for details."
-	fi
-fi
+Log "$PROGRAM:$WORKFLOW:DB:Backup(${BKP_ID}):${CLI_NAME}: Registering DR backup to DRLS database .... Success!
--- usr/share/drls/backup/run/custom/208_clear_older_backups.sh
+++ usr/share/drls/backup/run/custom/208_clear_older_backups.sh
@@ -1,17 +1,14 @@
-Log "Starting DR archive clean ..."
+Log "$PROGRAM:$WORKFLOW:ARCHIVE:CLEAN:${CLI_NAME}: DR Archive Clean in progress .... "
 
-N_BKP=$(grep -w ${CLI_NAME} ${BKPDB} | wc -l)
-if [ ${N_BKP} -gt ${HISTBKPMAX} ]
+if clean_backups ;
 then
-	BKPID2CLR=$(grep -w ${CLI_NAME} ${BKPDB} | awk -F":" '{print $1}' | sort -n | head -1)
-	F_DR2CLR=$(grep -w ${BKPID2CLR} ${BKPDB} | awk -F":" '{print $3}')
-
-	Log "DR backup (ID: ${BKPID2CLR}) marked for remove ..."
-
-	rm -vf ${ARCHDIR}/${F_DR2CLR}
-	ex -s -c ":g/^${BKPID2CLR}/d" -c ":wq" ${BKPDB}
-	Log "Old DR backups for client ${CLI_NAME} Removed Succesfully!"
+	Log "$PROGRAM:$WORKFLOW:ARCHIVE:DR:CLEAN:FS:DB:${CLI_NAME}: .... Success!"
+else
+	report_error "ERROR:$PROGRAM:$WORKFLOW:ARCHIVE:DR:CLEAN:FS:DB:${CLI_NAME}: Problem removing Oldest backup! aborting ..."
+	Error "$PROGRAM:$WORKFLOW:ARCHIVE:DR:CLEAN:FS:DB:${CLI_NAME}: Problem removing Oldest backup! aborting ..."
 fi
+
+Log "$PROGRAM:$WORKFLOW:ARCHIVE:CLEAN:${CLI_NAME}: DR Archive Clean in progress .... Success!"
 
 Log "####################################################"
 Log "# DR backup operations for ${CLI_NAME} finished!"
