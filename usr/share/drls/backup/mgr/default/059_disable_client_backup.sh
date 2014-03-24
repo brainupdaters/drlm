04ac2a2418e593a4da879d9eaabc77e8be47bc97
Didac Oliveira <didac@brainupdaters.net>
2014-03-24 15:44:33 +0100

    Improved Error handling in bkpmgr

--- usr/share/drls/backup/mgr/default/059_disable_client_backup.sh
+++ usr/share/drls/backup/mgr/default/059_disable_client_backup.sh
@@ -1,52 +1,65 @@
 
-Log "Deactivating previous DR store for client: ${CLI_NAME} ..."
+Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... "
 
 A_DR_FILE=$(losetup /dev/loop${CLI_ID} | grep -w "${CLI_NAME}" | awk '{print $3}' | tr -d "(" | tr -d ")")
 
 if [ -n "$A_DR_FILE" ]; then
 
-        disable_nfs_fs ${CLI_NAME}
-	#Error handling
-	if [ ! -d ${STORDIR}/${CLI_NAME} ]; then
-
-	        mkdir -v ${STORDIR}/${CLI_NAME}
-	        chmod 755 ${STORDIR}/${CLI_NAME}
-	fi
-
+        if disable_nfs_fs ${CLI_NAME} ;
+        then
+                Log "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: .... Success!"
+        else
+                Error "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: Problem disabling NFS export! aborting ..."
+        fi
 
         LO_MNT=$(mount -lt ext2 | grep -w "loop${CLI_ID}" | awk '{ print $3 }'| grep -w "${STORDIR}/${CLI_NAME}")
         if [ -n "$LO_MNT" ]
         then
-                do_umount ${CLI_ID}
-		#Error handling
+                if do_umount ${CLI_ID} ;
+		then
+                        Log "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): .... Success!"
+                else
+                        Error "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): Problem unmounting Filesystem! aborting ..."
+                fi
+
         fi
 
-        disable_loop ${CLI_ID}
+        if disable_loop ${CLI_ID} ;
+	then
+		Log "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: .... Success!"
+	else
+		Error "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: Problem disabling Loop Device! aborting ..."
+	fi
 
         if [ "$MODE" == "perm" ]; then
                 #A_BKP_ID_DB=$(grep -w ${CLI_NAME} ${BKPDB} | awk -F":" '{print $1,$5}'| grep -w "true" | awk '{print $1}')
 		A_BKP_ID_DB=$(get_active_cli_bkp_from_db ${CLI_NAME})
                 A_BKP_ID=$(echo ${A_DR_FILE} | awk -F"." '{print $2}')
                 if [ "$A_BKP_ID" == "$A_BKP_ID_DB" ] || [ "$ENABLE" == "yes" ]; then
-                        disable_backup_db ${A_BKP_ID_DB}
-                        if [ $? -eq 0 ]; then
-                                Log "Previous ${CLI_NAME} DR backup (ID: ${A_BKP_ID}) tagged as inactive in database ..."
+                        if disable_backup_db ${A_BKP_ID_DB} ;
+                        then
+                                Log "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: .... Success!"
                         else
-                                Error "Previous ${CLI_NAME} DR backup (ID: ${A_BKP_ID}) can not be tagged as inactive! Failed!"
+                                Error "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: Problem disabling backup in database! aborting ..."
                         fi
                 else
-                        Log "Previous ${CLI_NAME} active DR backup (ID: ${A_BKP_ID}) not set default active in DB. No DB update needed ..."
+                        Log "$PROGRAM:$WORKFLOW:MODE:perm:DB:(ID: ${A_BKP_ID}):${CLI_NAME}: Not set as default active in DB. No DB updates needed!"
                 fi
         fi
 
 fi
 
 if [ "$DISABLE" == "yes" ]; then
-        enable_nfs_fs_rw ${CLI_NAME}
-	#Error handling
-        Log "Finished Deactivating DR store for client: ${CLI_NAME} ..."
+        if enable_nfs_fs_rw ${CLI_NAME} ;
+        then
+                Log "$PROGRAM:$WORKFLOW:NFS:ENABLE (rw):$CLI_NAME: .... Success!"
+        else
+                Error "$PROGRAM:$WORKFLOW:NFS:ENABLE (rw):$CLI_NAME: Problem enabling NFS export (rw)! aborting ..."
+        fi
+
+	Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
         exit 0
 fi
 
-Log "Finished Deactivating DR store for client: ${CLI_NAME} ..."
+Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
 
