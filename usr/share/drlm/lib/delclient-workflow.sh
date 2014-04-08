51ca9b117ba3a125a7da447e72c67b4561e76b98
proura <pau@brainupdaters.net>
2014-04-08 22:22:49 +0200

    Update delclient-workflow.sh

--- usr/share/drlm/lib/delclient-workflow.sh
+++ usr/share/drlm/lib/delclient-workflow.sh
@@ -77,7 +77,7 @@ LOCKLESS_WORKFLOWS=( ${LOCKLESS_WORKFLOWS[@]} delclient )
 	fi
 #fi
 
-if [ -z "$CLI_NAME" ] || [ -z "$CLI_ID" ]; then
+if [ -z "$CLI_NAME" ] && [ -z "$CLI_ID" ]; then
 	echo "$PROGRAM $WORKFLOW: there are no all parameters required to run the command."
 	echo "Try \`$PROGRAM $WORKFLOW --help' for more information."
 	exit 1
