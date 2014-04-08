eadda36ad601f4d91ac3d3a6ee1fad4fce821fc6
proura <pau@brainupdaters.net>
2014-04-08 18:54:05 +0200

    delnetwork parameters control

--- usr/share/drlm/lib/delnetwork-workflow.sh
+++ usr/share/drlm/lib/delnetwork-workflow.sh
@@ -76,7 +76,7 @@ LOCKLESS_WORKFLOWS=( ${LOCKLESS_WORKFLOWS[@]} delnetwork )
 	fi
 #fi
 
-if [ -z "$NET_NAME" ] || [ -z "NET_ID" ]; then
+if [ -z "$NET_NAME" ] || [ -z "$NET_ID" ]; then
 	echo "$PROGRAM $WORKFLOW: there are no all parameters required to run the command."
 	echo "Try \`$PROGRAM $WORKFLOW --help' for more information."
 	exit 1
