aaba7f6dd8a608e2f8f26c1d159d70775cad1e86
Didac Oliveira <didac@brainupdaters.net>
2014-05-08 21:51:38 +0200

    Update 019_check_params.sh
    Log improvements

--- usr/share/drlm/backup/mgr/default/019_check_params.sh
+++ usr/share/drlm/backup/mgr/default/019_check_params.sh
@@ -1,28 +1,24 @@
-Log "####################################################"
-Log "# Starting DR backup operations for ${CLI_NAME}     "
-Log "####################################################"
-
 
 # Check if the target client is in DRLM client database
 if test -n "$CLI_NAME"; then
-	Log "Checking if client name: ${CLI_NAME} is registered in DRLM database ..."
+	Log "$PROGRAM:$WORKFLOW: Checking if client name: ${CLI_NAME} is registered in DRLM database ..."
 	if exist_client_name "$CLI_NAME" ;	
 	then
 		CLI_ID=$(get_client_id_by_name $CLI_NAME)
-		Log "${CLI_NAME} found in DRLM database!"
+		Log "$PROGRAM:$WORKFLOW: Client $CLI_NAME found!"
 	else
-		Error "$PROGRAM: Client named: $CLI_NAME not registered!"
+		Error "$PROGRAM:$WORKFLOW: Client $CLI_NAME not foundd!"
 	fi
 fi
 
 # Check if the target backup ID is in DRLM database
 if test -n "$BKP_ID"; then
-        Log "Checking if Backup ID: ${BKP_ID} is registered in DRLM database ..."
+        Log "$PROGRAM:$WORKFLOW: Checking if Backup ID: ${BKP_ID} is registered in DRLM database ..."
         if exist_backup_id "$BKP_ID" ;
         then
-                Log "${BKP_ID} found in DRLM database!"
+                Log "$PROGRAM:$WORKFLOW: Backup ID $BKP_ID found!"
         else
-                Error "$PROGRAM: Backup ID: $BKP_ID not registered!"
+                Error "$PROGRAM:$WORKFLOW: Backup ID $BKP_ID not found!"
         fi
 fi
 
