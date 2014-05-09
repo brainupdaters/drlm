66a44f37c1bc971cb06fdecba638401f39413f40
Didac Oliveira <didac@brainupdaters.net>
2014-05-10 00:55:13 +0200

    Update 019_check_client_exists.sh

--- usr/share/drlm/client/mod/default/019_check_client_exists.sh
+++ usr/share/drlm/client/mod/default/019_check_client_exists.sh
@@ -1,27 +1,30 @@
-Log "####################################################"
-Log "# Modifying client DR ${CLI_ID}${CLI_NAME}"
-Log "####################################################"
-
+Log "------------------------------------------------------------------"
+Log "$PROGRAM $WORWFLOW:                                               "
+Log "                                                                  "
+Log " - Modifying Client properties for $CLINAME                       "
+Log "                                                                  "
+Log " - Start Date & Time: $DATE                                       "
+Log "------------------------------------------------------------------"
 
 # Check if the client is in DRLM client database
 if test -n "$CLI_NAME"; then
-        Log "Checking if client name: ${CLI_NAME} is registered in DRLM database ..."
+        Log "$PROGRAM:$WORWFLOW: Searching client $CLI_NAME in DB ..."
         if exist_client_name "$CLI_NAME" ;
         then
                 CLI_ID=$(get_client_id_by_name $CLI_NAME)
-                Log "${CLI_NAME} found in DRLM database!"
+                Log "$PROGRAM:$WORWFLOW: Client $CLI_NAME found!"
         else
-                report_error "$PROGRAM: Client named: $CLI_NAME not registered!"
-                Error "$PROGRAM: Client named: $CLI_NAME not registered!"
+                #report_error "$PROGRAM:$WORWFLOW: Client $CLI_NAME not registered!"
+                Error "$PROGRAM:$WORWFLOW: Client $CLI_NAME not found! See $LOGFILE for details."
         fi
 else
-        Log "Checking if client ID: ${CLI_ID} is registered in DRLM database ..."
+        Log "$PROGRAM:$WORWFLOW: Searching client ID: $CLI_ID in DB ..."
         if exist_client_id "$CLI_ID" ;
         then
                 CLI_NAME=$(get_client_name $CLI_ID)
-                Log "${CLI_ID} found in DRLM database!"
+                Log "$PROGRAM:$WORWFLOW: Client ID: $CLI_ID found!"
         else
-                report_error "$PROGRAM: Client with ID: $CLI_ID not registered!"
-                Error "$PROGRAM: Client with ID: $CLI_ID not registered!"
+                #report_error "$PROGRAM: Client with ID: $CLI_ID not registered!"
+                Error "$PROGRAM:$WORWFLOW: Client ID: $CLI_ID not found! See $LOGFILE for details."
         fi
 fi
