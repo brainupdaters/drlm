8e04f9f9b5ce7388e35aedf38b15e26cd851607b
Didac Oliveira <didac@brainupdaters.net>
2014-05-10 01:50:14 +0200

    Update 019_check_network_exists.sh

--- usr/share/drlm/network/mod/default/019_check_network_exists.sh
+++ usr/share/drlm/network/mod/default/019_check_network_exists.sh
@@ -1,6 +1,13 @@
 Log "####################################################"
 Log "# Modifing Network ${NET_ID}${NET_NAME} from DRLM "
 Log "####################################################"
+Log "------------------------------------------------------------------"
+Log "$PROGRAM $WORWFLOW:                                               "
+Log "                                                                  "
+Log " - Modifying DR Network $NET_NAME from DRLM ...                   "
+Log "                                                                  "
+Log " - Start Date & Time: $DATE                                       "
+Log "------------------------------------------------------------------"
 
 
 # Check if the network is in DRLM network database
@@ -11,7 +18,7 @@ if test -n "$NET_NAME"; then
                 NET_ID=$(get_network_id_by_name $NET_NAME)
                 Log "${NET_NAME} found in DRLM database!"
         else
-                report_error "$PROGRAM: Network named: $NET_NAME not registered!"
+                #report_error "$PROGRAM: Network named: $NET_NAME not registered!"
                 Error "$PROGRAM: Network named: $NET_NAME not registered!"
         fi
 else
@@ -21,7 +28,7 @@ else
                 NET_NAME=$(get_network_name $NET_ID)
                 Log "${NET_ID} found in DRLM database!"
         else
-                report_error "$PROGRAM: Network with ID: $NET_ID not registered!"
+                #report_error "$PROGRAM: Network with ID: $NET_ID not registered!"
                 Error "$PROGRAM: Network with ID: $NET_ID not registered!"
         fi
 
