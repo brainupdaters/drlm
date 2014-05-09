fb85141389c509f19a2553708744e0c9039a62ee
Didac Oliveira <didac@brainupdaters.net>
2014-05-10 00:42:33 +0200

    Update 209_gen_srv_cfg_files_from_db.sh

--- usr/share/drlm/client/del/default/209_gen_srv_cfg_files_from_db.sh
+++ usr/share/drlm/client/del/default/209_gen_srv_cfg_files_from_db.sh
@@ -1,13 +1,13 @@
-Log "$PROGRAM:$WORKFLOW: Updating $HOSTS_FILE configuration from DRLM DB...."
+Log "$PROGRAM:$WORKFLOW: Updating $HOSTS_FILE configuration ..."
 
 if $(hosts_del $CLI_NAME $CLI_IP) ; then
-	Log "$PROGRAM:$WORKFLOW: $CLI_NAME deleted from $HOSTS_FILE..." 
+	Log "$PROGRAM:$WORKFLOW: Client $CLI_NAME deleted!" 
 else
-	Log "WARNING: $PROGRAM:$WORKFLOW: Problem deleting $CLI_NAME from $HOSTS_FILE..."
+	Log "WARNING: $PROGRAM:$WORKFLOW: Problem deleting $CLI_NAME from $HOSTS_FILE !"
 fi
 
 
-Log "$PROGRAM:$WORKFLOW: Populating DHCP configuration from DRLM DB...."
+Log "$PROGRAM:$WORKFLOW: Populating DHCP configuration ..."
 
 generate_dhcp
 
@@ -17,7 +17,7 @@ else
 	Error "$PROGRAM:$WORKFLOW: DHCP service reconfiguration failed! See $LOGFILE for details."
 fi
 
-Log "$PROGRAM:$WORKFLOW: Updating NFS configuration from DRLM DB...."
+Log "$PROGRAM:$WORKFLOW: Updating NFS configuration ..."
 
 #generate_nfs_exports
 
@@ -39,7 +39,10 @@ fi
 
 rmdir $STORDIR/$CLI_NAME
 
-Log "################################################"
-Log "# Client: $CLI_NAME deleted from DRLM!          "
-Log "################################################"
-
+Log "------------------------------------------------------------------"
+Log "$PROGRAM $WORWFLOW:                                               "
+Log "                                                                  "
+Log " - Deleting Client $CLINAME from DRLM ... Success!                "
+Log "                                                                  "
+Log " - End Date & Time: $DATE                                         "
+Log "------------------------------------------------------------------"
