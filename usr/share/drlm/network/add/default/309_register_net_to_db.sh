d3b425e9d266003aa5d1aa8c4a3e66e5a221da19
Didac Oliveira <didac@brainupdaters.net>
2014-05-10 01:35:41 +0200

    Update 309_register_net_to_db.sh

--- usr/share/drlm/network/add/default/309_register_net_to_db.sh
+++ usr/share/drlm/network/add/default/309_register_net_to_db.sh
@@ -1,15 +1,18 @@
-Log "####################################################"
-Log "#  Registering DR Network for ${NET_NAME}"
-Log "####################################################"
 
-Log     "Adding client to database $NETDB"
+Log "$PROGRAM:$WORWFLOW: Adding Network to database ($NETDB) ..."
 
 if add_network "$NET_IP" "$NET_MASK" "$NET_GW" "$NET_DOM" "$NET_DNS" "$NET_BCAST" "$NET_SRV" "$NET_NAME"
 then
-        Log "Network name: $NET_NAME has been registered on the database!"
+        Log "$PROGRAM:$WORWFLOW: Network $NET_NAME registation Success!"
 else
-        Error "Network: ERROR registering network $NET_NAME on the database!"
+        Error "$PROGRAM:$WORWFLOW: Problem registering network $NET_NAME to database! See $LOGFILE for details."
 fi
 
 
-
+Log "------------------------------------------------------------------"
+Log "$PROGRAM $WORWFLOW:                                               "
+Log "                                                                  "
+Log " - Registering DR Network $NET_NAME to DRLM ... Success!          "
+Log "                                                                  "
+Log " - End Date & Time: $DATE                                         "
+Log "------------------------------------------------------------------"
