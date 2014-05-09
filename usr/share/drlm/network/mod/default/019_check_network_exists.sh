eecbd3640f66130a532d271912bdb6559dc517cd
Didac Oliveira <didac@brainupdaters.net>
2014-05-10 01:52:03 +0200

    Update 019_check_network_exists.sh

--- usr/share/drlm/network/mod/default/019_check_network_exists.sh
+++ usr/share/drlm/network/mod/default/019_check_network_exists.sh
@@ -1,10 +1,8 @@
-Log "####################################################"
-Log "# Modifing Network ${NET_ID}${NET_NAME} from DRLM "
-Log "####################################################"
+
 Log "------------------------------------------------------------------"
-Log "$PROGRAM $WORWFLOW:                                               "
+Log "$PROGRAM $WORKFLOW:                                               "
 Log "                                                                  "
-Log " - Modifying DR Network $NET_NAME from DRLM ...                   "
+Log " - Modifying DR Network $NET_ID$NET_NAME from DRLM ...            "
 Log "                                                                  "
 Log " - Start Date & Time: $DATE                                       "
 Log "------------------------------------------------------------------"
@@ -16,20 +14,20 @@ if test -n "$NET_NAME"; then
         if exist_network_name "$NET_NAME" ;
         then
                 NET_ID=$(get_network_id_by_name $NET_NAME)
-                Log "${NET_NAME} found in DRLM database!"
+                Log "$PROGRAM:$WORKFLOW: ${NET_NAME} found in DRLM database!"
         else
                 #report_error "$PROGRAM: Network named: $NET_NAME not registered!"
-                Error "$PROGRAM: Network named: $NET_NAME not registered!"
+                Error "$PROGRAM:$WORKFLOW: Network named: $NET_NAME not registered!"
         fi
 else
         Log "Checking if network ID: ${NET_ID} is registered in DRLM database ..."
         if exist_network_id "$NET_ID" ;
         then
                 NET_NAME=$(get_network_name $NET_ID)
-                Log "${NET_ID} found in DRLM database!"
+                Log "$PROGRAM:$WORKFLOW: ${NET_ID} found in DRLM database!"
         else
                 #report_error "$PROGRAM: Network with ID: $NET_ID not registered!"
-                Error "$PROGRAM: Network with ID: $NET_ID not registered!"
+                Error "$PROGRAM:$WORKFLOW: Network with ID: $NET_ID not registered!"
         fi
 
 fi
