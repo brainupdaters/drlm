94c5e7d76965504a299c8bc15d2916337e439a28
Didac Oliveira <didac@brainupdaters.net>
2014-05-10 00:47:07 +0200

    Update 359_register_client_to_db.sh

--- usr/share/drlm/client/add/default/359_register_client_to_db.sh
+++ usr/share/drlm/client/add/default/359_register_client_to_db.sh
@@ -5,7 +5,7 @@ if add_client "$CLI_NAME" "$CLI_MAC" "$CLI_IP" "$CLI_OS" "$CLI_NET" ;
 then
 	Log "$PROGRAM:$WORKFLOW: Client name: $CLI_NAME registration Success!"
 else
-	Error "$PROGRAM:$WORKFLOW: Client: Problem registering client $CLI_NAME to DB!"
+	Error "$PROGRAM:$WORKFLOW: Client: Problem registering client $CLI_NAME to DB! See $LOGFILE for details."
 fi
 
 
