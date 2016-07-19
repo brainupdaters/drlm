223c7dd6c2038104a4db19086e14c9f8fc28ecab
krbu <ruben@brainupdaters.net>
2016-07-19 16:33:37 +0200

    add RH5 Debian6 Clients in installclient

--- usr/share/drlm/client/inst/default/359_install_client_soft.sh
+++ usr/share/drlm/client/inst/default/359_install_client_soft.sh
@@ -5,10 +5,14 @@ Log "####################################################"
 case ${DISTRO} in
   Debian)
     case ${VERSION} in
-               [7*-8*])
+               [6*-8*])
                         if check_apt ${USER} ${CLI_NAME} ${SUDO}
                         then
                             LogPrint "Installing dependendies and ReaR"
+                            if [[ ${VERSION} == 6 ]]
+                            then
+                                if install_dependencies_apt  ${USER} ${CLI_NAME} "${REAR_DEP_DEBIAN6}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            if
                             if [[ ${VERSION} == 7 ]]
                             then
                                 if install_dependencies_apt  ${USER} ${CLI_NAME} "${REAR_DEP_DEBIAN7}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
@@ -20,6 +24,10 @@ case ${DISTRO} in
                             if ssh_install_rear_dpkg ${USER} ${CLI_NAME} ${URL_REAR} ${SUDO}; then Log "ReaR has been installed"; else Error "Error installing ReaR, check logfile"; fi
                         else
                             Error "apt-get problem, some dependencies are missing, check requisites on http://drlm-docs.readthedocs.org/en/latest/ClientConfig.html"
+                        fi
+                        if [[ ${VERSION} == 6 ]]
+                        then
+                           if ssh_start_services ${USER} ${CLI_NAME} "portmap" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
                         fi
                         if [[ ${VERSION} == 7 ]]
                         then
@@ -41,6 +49,10 @@ case ${DISTRO} in
                         if check_yum ${USER} ${CLI_NAME} ${SUDO}
                         then
                             LogPrint "Installing dependendies and ReaR"
+                            if [[ ${VERSION} == 5 ]]
+                            then
+                                if install_dependencies_yum  ${USER} ${CLI_NAME} "${REAR_DEP_REDHAT5}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            fi
                             if [[ ${VERSION} == 6 ]]
                             then
                                 if install_dependencies_yum  ${USER} ${CLI_NAME} "${REAR_DEP_REDHAT6}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
@@ -57,6 +69,10 @@ case ${DISTRO} in
                             fi
                         else
                             Error "yum problem, some dependencies are missing, check requisites on http://drlm-docs.readthedocs.org/en/latest/ClientConfig.html"
+                        fi
+                        if [[ ${VERSION} == 5 ]]
+                        then
+                           if ssh_start_services ${USER} ${CLI_NAME} "portmap nfs" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
                         fi
                         if [[ ${VERSION} == 6 ]]
                         then
