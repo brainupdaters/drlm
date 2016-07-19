06608a2a474f084f235a955fbc623c4c51eb6d2c
krbu <ruben@brainupdaters.net>
2016-07-19 12:26:03 +0200

    separate packages and services by OS version

--- usr/share/drlm/client/inst/default/359_install_client_soft.sh
+++ usr/share/drlm/client/inst/default/359_install_client_soft.sh
@@ -5,16 +5,30 @@ Log "####################################################"
 case ${DISTRO} in
   Debian)
     case ${VERSION} in
-               [6*-8*])
+               [7*-8*])
                         if check_apt ${USER} ${CLI_NAME} ${SUDO}
                         then
                             LogPrint "Installing dependendies and ReaR"
-                            if install_dependencies_apt  ${USER} ${CLI_NAME} "${REAR_DEP_DEBIAN}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            if [[ ${VERSION} == 7 ]]
+                            then
+                                if install_dependencies_apt  ${USER} ${CLI_NAME} "${REAR_DEP_DEBIAN7}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            fi
+                            if [[ ${VERSION} == 8 ]]
+                            then
+                                if install_dependencies_apt  ${USER} ${CLI_NAME} "${REAR_DEP_DEBIAN8}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            fi
                             if ssh_install_rear_dpkg ${USER} ${CLI_NAME} ${URL_REAR} ${SUDO}; then Log "ReaR has been installed"; else Error "Error installing ReaR, check logfile"; fi
                         else
                             Error "apt-get problem, some dependencies are missing, check requisites on http://drlm-docs.readthedocs.org/en/latest/ClientConfig.html"
                         fi
-                        if ssh_start_services ${USER} ${CLI_NAME} "portmap" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
+                        if [[ ${VERSION} == 7 ]]
+                        then
+                           if ssh_start_services ${USER} ${CLI_NAME} "rpcbind" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
+                        fi
+                        if [[ ${VERSION} == 8 ]]
+                        then
+                           if ssh_start_services ${USER} ${CLI_NAME} "rpcbind" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
+                        fi
                         ;;
                 *)
                         echo "Release OS not identified!"
@@ -23,11 +37,18 @@ case ${DISTRO} in
     ;;
   CentOS|RedHat)
     case ${VERSION} in
-                [5*-7*])
+                [6*-7*])
                         if check_yum ${USER} ${CLI_NAME} ${SUDO}
                         then
                             LogPrint "Installing dependendies and ReaR"
-                            if install_dependencies_yum  ${USER} ${CLI_NAME} "${REAR_DEP_REDHAT}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            if [[ ${VERSION} == 6 ]]
+                            then
+                                if install_dependencies_yum  ${USER} ${CLI_NAME} "${REAR_DEP_REDHAT6}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            fi
+                            if [[ ${VERSION} == 7 ]]
+                            then
+                                if install_dependencies_yum  ${USER} ${CLI_NAME} "${REAR_DEP_REDHAT7}" ${SUDO}; then Log "Dependencies has been installed"; else Error "Error installing dependencies, check logfile"; fi
+                            fi
                             if [[ ${URL_REAR} == "" ]]
                             then
                                         if install_rear_yum_repo ${USER} ${CLI_NAME} ${SUDO}; then Log "ReaR has been installed from repo"; else Error "Error installing ReaR from repo, check logfile"; fi
@@ -37,7 +58,14 @@ case ${DISTRO} in
                         else
                             Error "yum problem, some dependencies are missing, check requisites on http://drlm-docs.readthedocs.org/en/latest/ClientConfig.html"
                         fi
-                        if ssh_start_services ${USER} ${CLI_NAME} "rpcbind nfs" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
+                        if [[ ${VERSION} == 6 ]]
+                        then
+                           if ssh_start_services ${USER} ${CLI_NAME} "rpcbind nfs" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
+                        fi
+                        if [[ ${VERSION} == 7 ]]
+                        then
+                           if ssh_start_services ${USER} ${CLI_NAME} "rpcbind" ${DISTRO} ${SUDO}; then LogPrint "Services has been started succesfully"; else "ERROR starting services"; fi
+                        fi
                         ;;
                 *)
                         echo "Release not identified!"
@@ -72,4 +100,5 @@ case ${DISTRO} in
         echo "Distribution not identified"
         ;;
 esac
+
 
