7463f3de0f7e373e7c378ab7b469cbb29e0c3972
Didac Oliveira <didac@brainupdaters.net>
2017-01-25 23:13:47 +0100

    Rollback to correct PID control that I broke up in a previous commit by mistake, issue #39

--- usr/share/drlm/backup/imp/default/029_pid_control.sh
+++ usr/share/drlm/backup/imp/default/029_pid_control.sh
@@ -10,7 +10,13 @@ if [ -f $VAR_DIR/run/$CLI_NAME.pid ]; then
     COMMAND=$( ps -p $CLI_PID -o comm= )
     if [ -n "$COMMAND" ]; then
       LogPrint "$PROGRAM:$WORKFLOW:Backup of client [ $CLI_NAME ] running with PID: $CLI_PID Command: $COMMAND."
-      Error "$PROGRAM:$WORKFLOW:Skipping impbackup of client [ $CLI_NAME ]."
+      Error "$PROGRAM:$WORKFLOW:Skipping import backup for client [ $CLI_NAME ]."
+    else
+      echo $BASHPID > $VAR_DIR/run/$CLI_NAME.pid
+      Log "$PROGRAM:$WORKFLOW:Importing backup for client [ $CLI_NAME ] with PID: $BASHPID."
     fi
   fi
+else
+  echo $BASHPID > $VAR_DIR/run/$CLI_NAME.pid
+  Log "$PROGRAM:$WORKFLOW:Importing backup for client [ $CLI_NAME ] with PID: $BASHPID."
 fi
