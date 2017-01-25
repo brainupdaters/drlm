69e43997c296891e0d45f1eb75aa2482abcf662a
proura <pau@brainupdaters.net>
2017-01-25 13:34:33 +0100

    Problem importing to new client solved
    I have added new client control at impbackup workflow.

--- usr/share/drlm/backup/imp/default/059_disable_client_backup.sh
+++ usr/share/drlm/backup/imp/default/059_disable_client_backup.sh
@@ -1,33 +1,41 @@
 # impbackup workflow
+if [ ! -d ${STORDIR}/${CLI_NAME} ]; then
 
-Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... "
+	Log "Making DR store mountpoint for client: ${CLI_NAME} ..."
 
-A_DR_FILE=$(losetup /dev/loop${CLI_ID} | grep -w "${CLI_NAME}" | awk '{print $3}' | tr -d "(" | tr -d ")")
+	mkdir -v ${STORDIR}/${CLI_NAME}
+	chmod 755 ${STORDIR}/${CLI_NAME}
 
-if [ -n "$A_DR_FILE" ]; then
+else
+  Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... "
 
-  if disable_nfs_fs ${CLI_NAME} ; then
-    Log "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: .... Success!"
-  else
-    Error "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: Problem disabling NFS export! aborting ..."
-  fi
+  A_DR_FILE=$(losetup /dev/loop${CLI_ID} | grep -w "${CLI_NAME}" | awk '{print $3}' | tr -d "(" | tr -d ")")
 
-  LO_MNT=$(mount -lt ext2,ext4 | grep -w "loop${CLI_ID}" | awk '{ print $3 }'| grep -w "${STORDIR}/${CLI_NAME}")
+  if [ -n "$A_DR_FILE" ]; then
 
-  if [ -n "$LO_MNT" ]; then
-    if do_umount ${CLI_ID} ;then
-      Log "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): .... Success!"
+    if disable_nfs_fs ${CLI_NAME} ; then
+      Log "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: .... Success!"
     else
-      Error "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): Problem unmounting Filesystem! aborting ..."
+      Error "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: Problem disabling NFS export! aborting ..."
     fi
+
+    LO_MNT=$(mount -lt ext2,ext4 | grep -w "loop${CLI_ID}" | awk '{ print $3 }'| grep -w "${STORDIR}/${CLI_NAME}")
+
+    if [ -n "$LO_MNT" ]; then
+      if do_umount ${CLI_ID} ;then
+        Log "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): .... Success!"
+      else
+        Error "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): Problem unmounting Filesystem! aborting ..."
+      fi
+    fi
+
+    if disable_loop ${CLI_ID} ; then
+      Log "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: .... Success!"
+    else
+      Error "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: Problem disabling Loop Device! aborting ..."
+    fi
+
   fi
 
-  if disable_loop ${CLI_ID} ; then
-    Log "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: .... Success!"
-  else
-    Error "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: Problem disabling Loop Device! aborting ..."
-  fi
-
+  Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
 fi
-
-Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
