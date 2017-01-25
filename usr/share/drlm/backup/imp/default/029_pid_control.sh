de29032335262b2ceb5550c67cdd4706b614cd63
Didac Oliveira <didac@brainupdaters.net>
2017-01-25 15:53:16 +0100

    Improved messages on import for backup PID control, issue #39

--- usr/share/drlm/backup/imp/default/029_pid_control.sh
+++ usr/share/drlm/backup/imp/default/029_pid_control.sh
@@ -9,14 +9,8 @@ if [ -f $VAR_DIR/run/$CLI_NAME.pid ]; then
   if [ "$CLI_PID" != "" ]; then
     COMMAND=$( ps -p $CLI_PID -o comm= )
     if [ -n "$COMMAND" ]; then
-      LogPrint "$PROGRAM:$WORKFLOW:Backup of client [ $CLI_NAME ] already running with PID: $CLI_PID Command: $COMMAND."
-      Error "$PROGRAM:$WORKFLOW:Skipping runbackup of client [ $CLI_NAME ]."
-    else
-      echo $BASHPID > $VAR_DIR/run/$CLI_NAME.pid
-      Log "$PROGRAM:$WORKFLOW:Running backup for client [ $CLI_NAME ] with PID: $BASHPID."
+      LogPrint "$PROGRAM:$WORKFLOW:Backup of client [ $CLI_NAME ] running with PID: $CLI_PID Command: $COMMAND."
+      Error "$PROGRAM:$WORKFLOW:Skipping impbackup of client [ $CLI_NAME ]."
     fi
   fi
-else
-  echo $BASHPID > $VAR_DIR/run/$CLI_NAME.pid
-  Log "$PROGRAM:$WORKFLOW:Running backup for client [ $CLI_NAME ] with PID: $BASHPID."
 fi
