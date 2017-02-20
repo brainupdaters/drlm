6e9cb9f029a26032ed2159b0e92e24871209c02c
proura <pau@brainupdaters.net>
2017-02-20 17:52:14 +0100

    solutions for issue #40 and #49

--- usr/share/drlm/backup/del/default/019_check_params.sh
+++ usr/share/drlm/backup/del/default/019_check_params.sh
@@ -1,31 +1,27 @@
 
-# Check if the target client is in DRLM client database
-if test -n "$CLI_NAME"; then
-        Log "Checking if client name: ${CLI_NAME} is registered in DRLM database ..."
-        if exist_client_name "$CLI_NAME" ;
-        then
-                CLI_ID=$(get_client_id_by_name $CLI_NAME)
-                Log "${CLI_NAME} found in DRLM database!"
-        else
-                Error "$PROGRAM: Client named: $CLI_NAME not registered!"
-        fi
-fi
-
 if [ -z "$CLEAN_ALL" ]; then
-
-# Check if the target backup ID is in DRLM database
-if test -n "$BKP_ID"; then
-        Log "Checking if Backup ID: ${BKP_ID} is registered in DRLM database ..."
-        if exist_backup_id "$BKP_ID" ;
-        then
-        	ID_LIST="$BKP_ID"
-                Log "${BKP_ID} found in DRLM database!"
-        else
-                Error "$PROGRAM: Backup ID: $BKP_ID not registered!"
+        # Check if the target backup ID is in DRLM database
+        if test -n "$BKP_ID"; then
+                Log "Checking if Backup ID: ${BKP_ID} is registered in DRLM database ..."
+                if exist_backup_id "$BKP_ID" ;
+                then
+                	ID_LIST="$BKP_ID"
+                        Log "${BKP_ID} found in DRLM database!"
+                else
+                        Error "$PROGRAM: Backup ID: $BKP_ID not registered!"
+                fi
         fi
-fi
-
 else
-
-	ID_LIST=$(get_backup_id_lst_by_client $CLI_NAME)
+        if test -n "$CLI_NAME"; then
+                # Check if the target client is in DRLM client database
+                Log "Checking if client name: ${CLI_NAME} is registered in DRLM database ..."
+                if exist_client_name "$CLI_NAME" ;
+                then
+                        CLI_ID=$(get_client_id_by_name $CLI_NAME)
+                        ID_LIST=$(get_backup_id_lst_by_client $CLI_NAME)
+                        Log "${CLI_NAME} found in DRLM database!"
+                else
+                        Error "$PROGRAM: Client named: $CLI_NAME not registered!"
+                fi
+        fi
 fi
--- usr/share/drlm/backup/del/default/209_delete_backups.sh
+++ usr/share/drlm/backup/del/default/209_delete_backups.sh
@@ -4,7 +4,7 @@ Log "$PROGRAM:$WORKFLOW:$CLI_NAME: Removing client backup(s)! .... "
 for ID in $ID_LIST; do
 	if check_backup_state $ID ;
 	then
-		DR_FILE=$(gen_dr_file_name $CLI_NAME $ID)
+		DR_FILE=$(get_backup_drfile $ID)
 		if ! exist_dr_file_fs $DR_FILE ;
 		then
 			LogPrint "WARNING: $PROGRAM:$WORKFLOW:ID($ID):$CLI_NAME: Backup DR file not in FS! Removing only from DB .... "
