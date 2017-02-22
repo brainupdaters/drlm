570b43a61de493b127f9325d34d537bf9b904689
proura <pau@brainupdaters.net>
2017-02-22 01:44:18 +0100

    Some errors solved in bkpmgr

--- usr/share/drlm/backup/mgr/default/059_disable_client_backup.sh
+++ usr/share/drlm/backup/mgr/default/059_disable_client_backup.sh
@@ -1,33 +1,37 @@
 # bkpmgr workflow
 
-Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating Backup ${A_BKP_ID_DB} for client: .... "
-
 if disable_nfs_fs ${CLI_NAME} ; then
    Log "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: .... Success!"
 else
    Error "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: Problem disabling NFS export! aborting ..."
 fi
 
-LO_MNT=$(mount -lt ext2,ext4 | grep -w "loop${CLI_ID}" | awk '{ print $3 }'| grep -w "${STORDIR}/${CLI_NAME}")
-if [ -n "$LO_MNT" ]; then
-   if do_umount ${CLI_ID} ; then
-      Log "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): .... Success!"
-   else
-      Error "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): Problem unmounting Filesystem! aborting ..."
+if [ -n "$A_BKP_ID_DB" ] ; then
+   Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating Backup ${A_BKP_ID_DB} for client: .... "
+
+   LO_MNT=$(mount -lt ext2,ext4 | grep -w "loop${CLI_ID}" | awk '{ print $3 }'| grep -w "${STORDIR}/${CLI_NAME}")
+   if [ -n "$LO_MNT" ]; then
+      if do_umount ${CLI_ID} ; then
+         Log "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): .... Success!"
+      else
+         Error "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): Problem unmounting Filesystem! aborting ..."
+      fi
    fi
-fi
 
-if disable_loop ${CLI_ID} ; then
-   Log "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: .... Success!"
-else
-   Error "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: Problem disabling Loop Device! aborting ..."
-fi
+   if disable_loop ${CLI_ID} ; then
+      Log "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: .... Success!"
+   else
+      Error "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: Problem disabling Loop Device! aborting ..."
+   fi
 
-#Disable backaup from database
-if disable_backup_db ${A_BKP_ID_DB} ; then
-   Log "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: .... Success!"
-else
-   Error "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: Problem disabling backup in database! aborting ..."
+   #Disable backaup from database
+   if disable_backup_db ${A_BKP_ID_DB} ; then
+      Log "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: .... Success!"
+   else
+      Error "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: Problem disabling backup in database! aborting ..."
+   fi
+
+   Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
 fi
 
 if [ "$DISABLE" == "yes" ]; then
@@ -40,6 +44,3 @@ if [ "$DISABLE" == "yes" ]; then
    Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating Backup ${A_BKP_ID_DB} for client: .... Success!"
    exit 0
 fi
-
-Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
-
