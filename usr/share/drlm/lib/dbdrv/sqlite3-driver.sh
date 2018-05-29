726b68bca8c8ad2ba7d72005cca6583d9ccdf69d
proura <pau@brainupdaters.net>
2018-05-29 13:03:23 +0200

    Like Issue #69 but in NetworkId.

--- usr/share/drlm/lib/dbdrv/sqlite3-driver.sh
+++ usr/share/drlm/lib/dbdrv/sqlite3-driver.sh
@@ -207,13 +207,15 @@ function add_network_dbdrv ()
     local NET_SERVIP=$7
     local NET_NAME=$8
 
+    #To improve prevention of duplicated id when addnetwork are done simultaneously
+    sleep $(echo 0.$[ $RANDOM % 10 ]$[ $RANDOM % 10 ])
+
     NET_ID=$(echo "select count(*) from counters where idcounter='networks';" | sqlite3 -init <(echo .timeout $SQLITE_TIMEOUT) $DB_PATH)
     if [ $NET_ID -eq  0 ]; then
         NET_ID=$(echo "select ifnull(max(idnetwork)+1, 1) from networks;" | sqlite3 -init <(echo .timeout $SQLITE_TIMEOUT) $DB_PATH)
         echo "INSERT INTO counters (idcounter, value) VALUES ('networks', $NET_ID);" | sqlite3 -init <(echo .timeout $SQLITE_TIMEOUT) $DB_PATH
     else
-        NET_ID=$(echo "select ifnull(value+1, 1) from counters where idcounter='networks';" | sqlite3 -init <(echo .timeout $SQLITE_TIMEOUT) $DB_PATH)
-        echo "UPDATE counters set value=$NET_ID where idcounter='networks';" | sqlite3 -init <(echo .timeout $SQLITE_TIMEOUT) $DB_PATH
+        NET_ID=$(echo "select value+1 from counters where idcounter='networks'; UPDATE counters set value=(value+1) where idcounter='networks';" | sqlite3 -init <(echo .timeout $SQLITE_TIMEOUT) $DB_PATH)
     fi
 
     echo "INSERT INTO networks (idnetwork, netip, mask, gw, domain, dns, broadcast, serverip, netname) VALUES (${NET_ID}, '${NET_IP}', '${NET_MASK}', '${NET_GW}', '${NET_DOM}', '${NET_DNS}', '${NET_BRO}', '${NET_SERVIP}', '${NET_NAME}' );" | sqlite3 -init <(echo .timeout $SQLITE_TIMEOUT) $DB_PATH
