5e07f48bd77048a473f4a7e2f2bdae32f7049107
Néfix Estrada <nefixestrada@gmail.com>
2019-02-08 08:42:32 +0100

    First attempt for adding colors
    This commit adds the -p paramter. It enables to output a colorized
    version of the listbackup command. This colors depend on the
    "correctness" of the backup. E.g. if the backup duration is less than 2
    minutes, we can safetly think that the backup has failed

--- usr/share/drlm/backup/list/default/019_list_backups_by_client.sh
+++ usr/share/drlm/backup/list/default/019_list_backups_by_client.sh
@@ -1,15 +1,86 @@
 Log "####################################################"
 Log "# List of Backups : 	                         "
 Log "####################################################"
+
 CLI_ID=$(get_client_id_by_name $CLI_NAME)
-if ! exist_client_name "$CLI_NAME" 
-then
-	if [ "$CLI_NAME" == "all" ]
-	then
+
+function check_backup_size_status() {
+	size=$(awk '{print $7}')
+}
+
+function check_backup_time_status() {
+	duration=$(awk '{print $6}')
+
+	if [ "${duration:0:1}" == "-" ]; then
+		separator="."
+		count=0
+		read -ra addr <<< "$duration"
+		for i in ${addr[@]}; do
+				case "$count" in
+					0)
+						if [ "$i" != "0h" ]; then
+							echo -n "failed"
+							break
+						fi
+						;;
+					1)
+						if [ "$i" == "0m" ] || [ "$i" == "1m" ]; then
+							echo -n "failed"
+							break
+						elif [ "$i" == "2m" ] || [ "$i" == "3m" ]; then
+							echo -n "warning"
+						fi
+						;;
+				esac
+
+				count=$((count + 1))
+		done
+	fi
+}
+
+function set_color() {
+   color="$2"
+
+   red="\033[0;31m"
+   yellow="\033[1;33m"
+   no_color="\033[0m"
+
+   column=$(echo "$2" | cut -d" " -f $1)
+   
+   echo "$3" | awk '$'"$1"'="'"${!color}$column$no_color"'"'
+}
+
+if ! exist_client_name "$CLI_NAME"; then
+	if [ "$CLI_NAME" == "all" ]; then
+		if [ -z "$PRETTY" ]; then 
         	list_backup_all
+		else
+
+		fi
 	else
 		printf '%25s\n' "$(tput bold)$CLI_NAME$(tput sgr0) not found in database!!"	
 	fi
 else
-	list_backup $CLI_NAME
+	if [ -z "$PRETTY" ]; then 
+		list_backup $CLI_NAME
+	else
+		while read line; do
+			# Check time
+			bkp_time_status=$(check_backup_time_status "$line")
+			if [ bkp_time_status == "failed" ]; then
+				line=$(set_color 6 "red" "$line")
+			elif [ bkp_time_status == "warning" ]; then
+				line=$(set_color 6 "yellow" "$line")
+			fi
+
+			# Check size
+			bkp_size_status=$(check_backup_size_status "$line")
+			if [ bkp_size_status == "failed" ]; then
+				line=$(set_color 7, "red", "$line")
+			elif [ bkp_size_status == "unknown" ]; then
+				line=$(set_color 7, "yellow", "$line")
+			fi
+
+		done < list_backup $CLI_NAME
+	fi
 fi
