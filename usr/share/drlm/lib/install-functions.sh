1f8067e4fce399f1bfe1d353ad79abfa9829cdb0
Néfix Estrada <nefixestrada@gmail.com>
2019-05-15 09:55:05 +0200

    changes

--- usr/share/drlm/lib/install-functions.sh
+++ usr/share/drlm/lib/install-functions.sh
@@ -312,9 +312,12 @@ function copy_ssh_id () {
     local SUDO=$4
 
     PUBKEY=$(<~/.ssh/id_rsa.pub)
-    DRLM_USER_HOME_DIR=getent passwd "$DRLM_USER" | cut -d: -f6
 
-    ssh $SSH_OPTS $USER@$CLI_NAME "$SUDO mkdir $DRLM_USER_HOME_DIR/.ssh && $SUDO echo '$PUBKEY' >> $DRLM_USER_HOME_DIR/.ssh/authorized_keys"
+    ssh $SSH_OPTS $USER@$CLI_NAME "DRLM_USER_HOME_DIR=\"\$(getent passwd \"$DRLM_USER\" | cut -d: -f6)\" ;
+        if [ ! -d \"\$DRLM_USER_HOME_DIR/.ssh\" ]; then
+            $SUDO mkdir \"\$DRLM_USER_HOME_DIR/.ssh\"
+        fi ;
+        $SUDO echo '$PUBKEY' >> \$DRLM_USER_HOME_DIR/.ssh/authorized_keys"
 }
 
 function authors () {
