2b2596619f61e42d064629515fb5e4c34cee9839
db260179 <db260179@gmail.com>
2019-07-15 14:37:54 +0100

    Variable dockerdir in setup.sh

--- packaging/docker/setup.sh
+++ packaging/docker/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source env.conf
+DOCKER_DIR=${PWD}/packaging/docker
+
+source ${DOCKER_DIR}/env.conf
 
 echo "${0}: Build the docker images: ${DOCKER_IMAGE}:${DOCKER_TAG}"
 docker build --rm --compress -t ${DOCKER_IMAGE}:${DOCKER_TAG} ./src
