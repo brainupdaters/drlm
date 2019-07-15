1137b4cb5d84bcff217a03909264771a2593edca
db260179 <db260179@gmail.com>
2019-07-15 16:29:44 +0100

    Fix src location

--- packaging/docker/setup.sh
+++ packaging/docker/setup.sh
@@ -5,4 +5,4 @@ DOCKER_DIR=${PWD}/packaging/docker
 source ${DOCKER_DIR}/env.conf
 
 echo "${0}: Build the docker images: ${DOCKER_IMAGE}:${DOCKER_TAG}"
-docker build --rm --compress -t ${DOCKER_IMAGE}:${DOCKER_TAG} ./src
+docker build --rm --compress -t ${DOCKER_IMAGE}:${DOCKER_TAG} ${DOCKER_DIR}/src
