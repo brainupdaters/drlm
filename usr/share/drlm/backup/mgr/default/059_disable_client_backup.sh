ceff8c03dec0f5fa06ff8f988749ac251a6c7864
Alessandro Peyrachia <61762607+Peyoz@users.noreply.github.com>
2020-04-23 09:10:36 +0200

    Reworked NFS Functions (#142)
    * Reworked NFS Funcitons
    issue #138
    New NFS configuration function leverage a strict file based approach
    Every client has its own export file located under /etc/exports.d
    Messing with /etc/exports is totally avoided
    Current export state is always understandable by simply checking the files in /etc/exports.d
    Filename follow this pattern ${CLI_NAME}.drlm.exports
    Enabling an export is implemented by creating the file in /etc/exports.d
    Disabling an export is implemented prepending a '.' to the file
    drlm-stord now disables nfs exports at shutdown
    drlm-stord enables nfs exports only for active backups at startup
    
    * Fixed Makefile
    Removing drlm-api from git created a regression in creating drlm.tar.gz
    Tar archive is created listing the files tracked by the git branch, deleting the binary results results in install step failing as the file is not inside the tar archive used to create the rpm.
    drlm-api is specifically added to the tar archive

--- usr/share/drlm/backup/mgr/default/059_disable_client_backup.sh
+++ usr/share/drlm/backup/mgr/default/059_disable_client_backup.sh
@@ -1,46 +1,55 @@
 # bkpmgr workflow
+function wf_disable_client_backup(){
+   if [ ! -z ${1} ]; then
+      local A_BKP_ID_DB=${1}
+   fi
 
-if disable_nfs_fs ${CLI_NAME}; then
-   Log "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: .... Success!"
-else
-   Error "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: Problem disabling NFS export! aborting ..."
-fi
+   if disable_nfs_fs ${CLI_NAME} ; then
+      Log "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: .... Success!"
+   else
+      Error "$PROGRAM:$WORKFLOW:NFS:DISABLE:$CLI_NAME: Problem disabling NFS export! aborting ..."
+   fi
 
-if [ -n "$A_BKP_ID_DB" ]; then
-   Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating Backup ${A_BKP_ID_DB} for client: .... "
+   if [ -n "$A_BKP_ID_DB" ] ; then
+      Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating Backup ${A_BKP_ID_DB} for client: .... "
 
-   LO_MNT=$(mount -lt ext2,ext4 | grep -w "loop${CLI_ID}" | awk '{ print $3 }'| grep -w "${STORDIR}/${CLI_NAME}")
-   if [ -n "$LO_MNT" ]; then
-      if do_umount ${CLI_ID}; then
-         Log "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): .... Success!"
-      else
-         Error "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): Problem unmounting Filesystem! aborting ..."
+      LO_MNT=$(mount -lt ext2,ext4 | grep -w "loop${CLI_ID}" | awk '{ print $3 }'| grep -w "${STORDIR}/${CLI_NAME}")
+      if [ -n "$LO_MNT" ]; then
+         if do_umount ${CLI_ID} ; then
+            Log "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): .... Success!"
+         else
+            Error "$PROGRAM:$WORKFLOW:FS:UMOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): Problem unmounting Filesystem! aborting ..."
+         fi
       fi
+
+      if disable_loop ${CLI_ID} ; then
+         Log "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: .... Success!"
+      else
+         Error "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: Problem disabling Loop Device! aborting ..."
+      fi
+
+      #Disable backup from database
+      if disable_backup_db ${A_BKP_ID_DB} ; then
+         Log "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: .... Success!"
+      else
+         Error "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: Problem disabling backup in database! aborting ..."
+      fi
+
+      Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
    fi
+}
 
-   if disable_loop ${CLI_ID}; then
-      Log "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: .... Success!"
-   else
-      Error "$PROGRAM:$WORKFLOW:LOOPDEV(${CLI_ID}):DISABLE:$CLI_NAME: Problem disabling Loop Device! aborting ..."
-   fi
-
-   #Disable backaup from database
-   if disable_backup_db ${A_BKP_ID_DB}; then
-      Log "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: .... Success!"
-   else
-      Error "$PROGRAM:$WORKFLOW:MODE:perm:DB:disable:(ID: ${A_BKP_ID}):${CLI_NAME}: Problem disabling backup in database! aborting ..."
-   fi
-
-   Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating previous DR store for client: .... Success!"
-fi
-
-if [ "$DISABLE" == "yes" ]; then
-   if enable_nfs_fs_rw ${CLI_NAME}; then
-      Log "$PROGRAM:$WORKFLOW:NFS:ENABLE (rw):$CLI_NAME: .... Success!"
-   else
-      Error "$PROGRAM:$WORKFLOW:NFS:ENABLE (rw):$CLI_NAME: Problem enabling NFS export (rw)! aborting ..."
-   fi
-
+if [[ ${DISABLE} == 'yes' ]]; then
+   wf_disable_client_backup
    Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating Backup ${A_BKP_ID_DB} for client: .... Success!"
    exit 0
+fi
+if [[ ${ENABLE} == 'yes' ]]; then
+   local A_BKP_ID_DB_OLD=$(get_active_cli_bkp_from_db_dbdrv ${CLI_NAME})
+   if wf_disable_client_backup ${A_BKP_ID_DB_OLD} ; then
+      Log "$PROGRAM:$WORKFLOW:${CLI_NAME}: Deactivating Backup ${A_BKP_ID_DB} for client: .... Success!"
+      return 0
+   else
+      return 1
+   fi
 fi
