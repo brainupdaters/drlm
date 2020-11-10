003730cff3332bedc261d710e6c44499e26b3c93
proura <pau@brainupdaters.net>
2020-11-10 18:28:59 +0100

    Code improvements

--- usr/share/drlm/backup/run/default/129_post_backup_tasks.sh
+++ usr/share/drlm/backup/run/default/129_post_backup_tasks.sh
@@ -9,56 +9,56 @@ CLI_KERNEL_OPTS=$(grep -h -w append ${STORDIR}/${CLI_NAME}/PXE/${CLI_REAR_PXE_FI
 Log "$PROGRAM:$WORKFLOW:postbackup:${CLI_NAME}: Fixing PXE permissions for DR image ..."
 
 if [ $(stat -c %a ${STORDIR}/${CLI_NAME}) != "755" ]; then
-	chmod 755 ${STORDIR}/${CLI_NAME}
-	if [ $? -ne 0 ]; then
-		Error "chmod 755 ${STORDIR}/${CLI_NAME} failed!"
-	fi
+    chmod 755 ${STORDIR}/${CLI_NAME}
+    if [ $? -ne 0 ]; then
+        Error "chmod 755 ${STORDIR}/${CLI_NAME} failed!"
+    fi
 fi
 
 if [ $(stat -c %a ${STORDIR}/${CLI_NAME}/PXE) != "755" ]; then
-	chmod 755 ${STORDIR}/${CLI_NAME}/PXE
-	if [ $? -ne 0 ]; then
-		Error "chmod 755 ${STORDIR}/${CLI_NAME}/PXE failed!"
-	fi
+    chmod 755 ${STORDIR}/${CLI_NAME}/PXE
+    if [ $? -ne 0 ]; then
+        Error "chmod 755 ${STORDIR}/${CLI_NAME}/PXE failed!"
+    fi
 fi
 
 if [ $(stat -c %a ${STORDIR}/${CLI_NAME}/PXE/${CLI_KERNEL_FILE}) != "755" ]; then
-	chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_KERNEL_FILE}
-	if [ $? -ne 0 ]; then
-		Error "chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_KERNEL_FILE} failed!"
-	fi
+    chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_KERNEL_FILE}
+    if [ $? -ne 0 ]; then
+        Error "chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_KERNEL_FILE} failed!"
+    fi
 fi
 
 if [ $(stat -c %a ${STORDIR}/${CLI_NAME}/PXE/${CLI_INITRD_FILE}) != "755" ]; then
-        chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_INITRD_FILE}
-        if [ $? -ne 0 ]; then
-                Error "chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_INITRD_FILE} failed!"
-        fi
+    chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_INITRD_FILE}
+    if [ $? -ne 0 ]; then
+        Error "chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_INITRD_FILE} failed!"
+    fi
 fi
 
 if [ $(stat -c %a ${STORDIR}/${CLI_NAME}/PXE/${CLI_REAR_PXE_FILE}) != "755" ]; then
-        chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_REAR_PXE_FILE}
-        if [ $? -ne 0 ]; then
-                Log "WARNING:$PROGRAM:$WORKFLOW: chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_REAR_PXE_FILE} failed!"
-        fi
+    chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_REAR_PXE_FILE}
+    if [ $? -ne 0 ]; then
+        Log "WARNING:$PROGRAM:$WORKFLOW: chmod 755 ${STORDIR}/${CLI_NAME}/PXE/${CLI_REAR_PXE_FILE} failed!"
+    fi
 fi
 
 Log "$PROGRAM:$WORKFLOW:postbackup:${CLI_NAME}: Fixing PXE permissions for DR image ... Success!"
 
 Log "$PROGRAM:$WORKFLOW:postbackup:${CLI_NAME}: Enabling DRLM Store ..."
 
-	if do_remount ro ${CLI_ID} ${CLI_NAME};	then
-		Log "$PROGRAM:$WORKFLOW:postbackup:FS:MOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): ... Success!"
-		if enable_nfs_fs_ro ${CLI_NAME}; then
-			Log "$PROGRAM:$WORKFLOW:postbackup:NFS:ENABLE(ro):$CLI_NAME: ... Success!"
-		else
-			report_error "ERROR:$PROGRAM:$WORKFLOW:postbackup:NFS:ENABLE (ro):$CLI_NAME: Problem enabling NFS export (ro)! aborting ..."
-			Error "$PROGRAM:$WORKFLOW:postbackup:NFS:ENABLE (ro):$CLI_NAME: Problem enabling NFS export (ro)! aborting ..."
-		fi
-	else
-		report_error "ERROR:$PROGRAM:$WORKFLOW:postbackup:FS:MOUNT:LOOPDEV(${CLI_ID}):MNT(${STORDIR}/${CLI_NAME}): Problem mounting Filesystem!"
-		Error "$PROGRAM:$WORKFLOW:postbackup:FS:MOUNT:LOOPDEV(${CLI_ID}):MNT(${STORDIR}/${CLI_NAME}): Problem mounting Filesystem!"
-	fi
+if do_remount ro ${CLI_ID} ${CLI_NAME}; then
+    Log "$PROGRAM:$WORKFLOW:postbackup:FS:MOUNT:LOOPDEV(${CLI_ID}):MNT($STORDIR/$CLI_NAME): ... Success!"
+    if enable_nfs_fs_ro ${CLI_NAME}; then
+        Log "$PROGRAM:$WORKFLOW:postbackup:NFS:ENABLE(ro):$CLI_NAME: ... Success!"
+    else
+        report_error "ERROR:$PROGRAM:$WORKFLOW:postbackup:NFS:ENABLE (ro):$CLI_NAME: Problem enabling NFS export (ro)! aborting ..."
+        Error "$PROGRAM:$WORKFLOW:postbackup:NFS:ENABLE (ro):$CLI_NAME: Problem enabling NFS export (ro)! aborting ..."
+    fi
+else
+    report_error "ERROR:$PROGRAM:$WORKFLOW:postbackup:FS:MOUNT:LOOPDEV(${CLI_ID}):MNT(${STORDIR}/${CLI_NAME}): Problem mounting Filesystem!"
+    Error "$PROGRAM:$WORKFLOW:postbackup:FS:MOUNT:LOOPDEV(${CLI_ID}):MNT(${STORDIR}/${CLI_NAME}): Problem mounting Filesystem!"
+fi
 
 Log "$PROGRAM:$WORKFLOW:postbackup:${CLI_NAME}: Enabling DRLM Store .... Success!"
 
@@ -75,9 +75,7 @@ cat << EOF > ${STORDIR}/boot/cfg/${F_CLI_MAC}
 
 EOF
 
-test -f ${STORDIR}/boot/cfg/${F_CLI_MAC}
-
-if [ $? -eq 0 ]; then
+if [ -f ${STORDIR}/boot/cfg/${F_CLI_MAC} ]; then
     Log "$PROGRAM:$WORKFLOW:postbackup:PXE:${CLI_NAME}:Creating MAC Address (GRUB2) boot configuration file ... Success!"
 else
     report_error "ERROR:$PROGRAM:$WORKFLOW:postbackup:PXE:${CLI_NAME}: Problem Creating MAC Address (GRUB2) boot configuration file! aborting ..."
