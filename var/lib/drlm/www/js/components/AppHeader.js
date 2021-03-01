2f1193a0311d539b37a133921c54aa5c7eebc4d5
proura <pau@brainupdaters.net>
2021-03-01 12:37:45 +0100

    drlm-api front end reorganitzation (#161)
    * drlm-api front end reorganitzated

---
+++ var/lib/drlm/www/js/components/AppHeader.js
@@ -0,0 +1,14 @@
+var AppHeader = {
+  template: `
+    <div class="row">
+      <div class="col">
+        <img class="mb-4" src="static/drlm_long.png" alt="" width="250" height="131">
+      </div>
+      <div class="col">
+          <button type="button" class="btn btn-primary float-end" id="logout-button">Logout</button>
+      </div>
+    </div>
+  `
+}
+
+export default AppHeader;
---
+++ var/lib/drlm/www/js/components/ClientsTable.js
@@ -0,0 +1,48 @@
+import ClientsTableConfigurationsList from './ClientsTableConfigurationsList.js'
+
+var ClientsTable = {
+  components:{
+    'client-configurations-list': ClientsTableConfigurationsList
+  },
+  props: ['clients'],
+  template: `
+    <div class="table-responsive">
+      <table class="table table-hover" id="clients-table">
+        <caption>Clients</caption>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>MAC</th>
+            <th>Network</th>
+            <th>OS</th>
+            <th>ReaR</th>
+            <th>Token</th>
+            <th>Configs</th>
+          </tr>
+        </thead>
+        <tbody v-for="client in clients" v-bind:key="client.cli_id">
+          <tr>
+            <td>{{ client.cli_id }}</td>
+            <td>{{ client.cli_name }}</td>
+            <td>{{ client.cli_mac }}</td>
+            <td>{{ client.cli_net }}</td>
+            <td>{{ client.cli_os }}</td>
+            <td>{{ client.cli_rear }}</td>
+            <td>{{ client.cli_token }}</td>
+            <td>
+              <client-configurations-list
+                v-for="config in client.cli_configs"
+                v-bind:key="config.config_name"
+                v-bind:config="config"
+                v-bind:client="client"
+              ></client-configurations-list>
+            <td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `
+}
+
+export default ClientsTable;
