c6f70a219f96691655ba78b7b74779aa9151b3cc
proura <pau@brainupdaters.net>
2021-04-01 13:41:59 +0200

    typo correction

--- usr/share/drlm/conf/nbd/config-nbd.sh
+++ usr/share/drlm/conf/nbd/config-nbd.sh
@@ -7,12 +7,12 @@ fi
 case $1 in
   "install")
     if [ -f /etc/modprobe.d/nbd.conf ]; then
-      mv /etc/modprobe.d/nbd.conf /usr/share/drlm/conf/ndb/nbd.conf.options.save
+      mv /etc/modprobe.d/nbd.conf /usr/share/drlm/conf/nbd/nbd.conf.options.save
     fi
     echo "options nbd max_part=8 nbds_max=256" > /etc/modprobe.d/nbd.conf
 
     if [ -f /etc/modules-load.d/nbd.conf ]; then
-      mv /etc/modules-load.d/nbd.conf /usr/share/drlm/conf/ndb/nbd.conf.load.save
+      mv /etc/modules-load.d/nbd.conf /usr/share/drlm/conf/nbd/nbd.conf.load.save
     fi
     echo "nbd" > /etc/modules-load.d/nbd.conf
 
@@ -26,8 +26,8 @@ case $1 in
     fi
 
     # Restore configuration if exists
-    if [ -f /usr/share/drlm/conf/ndb/nbd.conf.options.save ]; then
-      mv /usr/share/drlm/conf/ndb/nbd.conf.options.save /etc/modprobe.d/nbd.conf
+    if [ -f /usr/share/drlm/conf/nbd/nbd.conf.options.save ]; then
+      mv /usr/share/drlm/conf/nbd/nbd.conf.options.save /etc/modprobe.d/nbd.conf
     fi
 
     # Remove current configuration
@@ -36,8 +36,8 @@ case $1 in
     fi
 
     # Restore configuration if exists
-    if [ -f /usr/share/drlm/conf/ndb/nbd.conf.load.save ]; then
-      mv /usr/share/drlm/conf/ndb/nbd.conf.load.save /etc/modules-load.d/nbd.conf
+    if [ -f /usr/share/drlm/conf/nbd/nbd.conf.load.save ]; then
+      mv /usr/share/drlm/conf/nbd/nbd.conf.load.save /etc/modules-load.d/nbd.conf
     else
       modprobe -r nbd
     fi  
