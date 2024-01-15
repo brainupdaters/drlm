d178b2ba0148d70d515a9cb696f108dcfd021b4e
Didac Oliveira <didac@drlm.org>
2024-01-15 10:43:07 +0100

    fix 98-drlm-setup-rescue.sh bug

--- usr/share/drlm/lib/install-functions.sh
+++ usr/share/drlm/lib/install-functions.sh
@@ -563,23 +563,23 @@ function tunning_rear () {
   $SUDO cat > /usr/share/rear/skel/default/etc/scripts/system-setup.d/98-drlm-setup-rescue.sh << EOF
 # Setting required environment for DRLM proper function
 
-is_true "$DRLM_MANAGED" || return 0
+is_true "\$DRLM_MANAGED" || return 0
 
 read -r </proc/cmdline
 
-echo $REPLY | grep -q "drlm="
-if [ $? -eq 0 ]; then
-    drlm_cmdline=( $(echo ${REPLY#*drlm=} | tr "," " ") )
-    for i in ${drlm_cmdline[@]}
+echo \$REPLY | grep -q "drlm="
+if [ \$? -eq 0 ]; then
+    drlm_cmdline=( \$(echo \${REPLY#*drlm=} | tr "," " ") )
+    for i in \${drlm_cmdline[@]}
     do
-        eval $i
+        eval \$i
     done
 
     echo "DRLM_MANAGED: Getting updated rescue configuration from DRLM ..."
 
-    test -n "$server" && echo "DRLM_SERVER=$server" >> /etc/rear/rescue.conf
-    test -n "$id" && echo "DRLM_ID=$id" >> /etc/rear/rescue.conf
-    test -n "$server" && echo 'DRLM_REST_OPTS="-H Authorization:$(cat /etc/rear/drlm.token) -k"' >> /etc/rear/rescue.conf
+    test -n "\$server" && echo "DRLM_SERVER=\$server" >> /etc/rear/rescue.conf
+    test -n "\$id" && echo "DRLM_ID=\$id" >> /etc/rear/rescue.conf
+    test -n "\$server" && echo 'DRLM_REST_OPTS="-H Authorization:\$(cat /etc/rear/drlm.token) -k"' >> /etc/rear/rescue.conf
 
 fi
 EOF
